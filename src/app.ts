acf0abcbe6303c5eb23488f03b2691b898e012e7
Julio Cesar Andres Campos Romero <69827308+JulioRom@users.noreply.github.com>
2022-07-31 21:47:30 -0400

    add path env

--- src/app.ts
+++ src/app.ts
@@ -2,24 +2,31 @@ import express from "express";
 import logger from "./utils/logger";
 import routes from "./routes/controller.routes";
 import swaggerDocs from "./utils/swagger";
-import { responseEnhancer } from 'express-response-formatter';
+import { responseEnhancer } from "express-response-formatter";
 import * as dotenv from "dotenv";
-import { NODE_ENV ,isProduction, isDevelopment } from "./utils/Environments/EnvironmentVar"
+import path from "path";
+import {
+  NODE_ENV,
+  isProduction,
+  isDevelopment,
+} from "./utils/Environments/EnvironmentVar";
 
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, "../.env.development") });
 
-logger.info({NODE_ENV})
-logger.info({isProduction, isDevelopment})
+logger.info({ NODE_ENV });
+logger.info({ isProduction, isDevelopment });
 
+const PORT = process.env.PORT;
+console.log(PORT);
 const app = express();
 
 app.use(express.json());
-app.use(responseEnhancer())
+app.use(responseEnhancer());
 
-app.listen(process.env.PORT, async () => {
-  logger.info(`App is running at http://localhost:${process.env.PORT}`);
-  
+app.listen(PORT, async () => {
+  logger.info(`App is running at http://localhost:${PORT}`);
+
   routes(app);
-  
-  swaggerDocs(app, process.env.PORT);
+
+  swaggerDocs(app, PORT);
 });
